33721d02a35b9d306ca82f08bcb876dcdd255649
KBS0123 <s3260626@gmail.com>
2024-07-01 11:56:16 +0900

    모임방 기능 추가

---
+++ Dahyang_project/src/main/java/spring_Dahyang/club/repository/ClubMapper.java
@@ -0,0 +1,25 @@
+package spring_Dahyang.club.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import spring_Dahyang.club.model.Club;
+
+public interface ClubMapper {
+	
+	public int insert(Club club);
+	public int update(Club club);
+	public int delete(int clid);
+	
+	@Select("select count(*) from dclub")
+	public int count();
+	
+	@Select("select * from dclub order by club_id desc")
+	public List<Club> selectAll();
+	
+	public Club selectById(int clid);
+	
+	public int deleteByUid(int uid);
+	
+}
--- Dahyang_project/src/main/java/spring_Dahyang/user/model/User.java
+++ Dahyang_project/src/main/java/spring_Dahyang/user/model/User.java
@@ -1,15 +1,20 @@
 package spring_Dahyang.user.model;
+
 import lombok.Data;
-
-import java.util.Date;
-
 import lombok.AllArgsConstructor; //모든 멤버변수를 입력받는 생성자
 import lombok.NoArgsConstructor; //디폴트 생성자 처리
+
+import java.util.List;
+
+import javax.persistence.OneToMany;
+
+import spring_Dahyang.club.model.Club;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+	
 	private int uid;
 	private String email;
 	private String pwd;
@@ -17,16 +22,7 @@ public class User {
 	private String birthday;
 	private String images;
 	
-	/* lombok에서 처리해주기 때문에 작성 불필요
-	 public User() {} 디폴트 생성자
-	 public User(String email, String password) {
-	 	this.email = email;
-	 	this.password = password;
-	 }
-	 public String setEmail(String email){}
-	 public void getEmail(){}
-	 public String setPassword(String password){}
-	 public void getPassword(){}
-	 */
-
+	@OneToMany(mappedBy = "creator")
+    private List<Club> clubs;
+	
 }
