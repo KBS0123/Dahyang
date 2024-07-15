37953c0d28e89d97e9fa377021e40e9cfc3a058d
KBS0123 <s3260626@gmail.com>
2024-07-15 09:16:04 +0900

    파일 저장 위치 수정

--- Dahyang_project/src/main/java/spring_Dahyang/file/FileService.java
+++ Dahyang_project/src/main/java/spring_Dahyang/file/FileService.java
@@ -6,7 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public interface FileService {
 
-	public static final String IMAGE_REPO="C:/springworks/Dahyang/Dahyang_project/src/main/webapp/resources/imgs";
+	public static final String IMAGE_REPO="C:/Users/hi-pc-999/git/Dahyang/Dahyang_project/src/main/webapp/resources/imgs";
 	public String saveFile(MultipartFile file);
 	
 }
